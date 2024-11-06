690645dd50008f3f1363e60baf8971f5bbfe8176
Kai Philipp <kphilipp@inquies.de>
2024-11-06 15:46:39 +0100

    missing flake.nix adaptions for build.nix

--- flake.nix
+++ flake.nix
@@ -5,13 +5,17 @@
   let
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
   in {
+    # This is our new package for end-users
+    packages.x86_64-linux.default = pkgs.qt6Packages.callPackage ./build.nix {};
     devShells.x86_64-linux.default = pkgs.mkShell {
       buildInputs = with pkgs; [
         cmake
-	ninja
+	      ninja
         gdb
         qt6.full
         qt6.qtbase
+        flatpak-builder
+        flatpak
 
         # this is for the shellhook portion
         qt6.wrapQtAppsHook
